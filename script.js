7e83a0fd6a7c3bfe0f0fc90a226a462d1bb1f4e4
IanWen103026 <114296116+IanWen103026@users.noreply.github.com>
2023-12-18 12:39:44 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -21,10 +21,13 @@ let XText = "X"
 document.getElementById("reset").style.display = "none";
 document.getElementById("inputer1").style.display = "none";
 document.getElementById("inputer2").style.display = "none";
-
+document.getElementById("h2").style.display = "none";
 
 function startGame(mode) {
   
+
+  
+
   OText = document.getElementById("inputer1").value
   XText = document.getElementById("inputer2").value
 
@@ -43,31 +46,37 @@ function startGame(mode) {
   document.getElementById("inputer2").style.display = "block";
   
 
-
+  document.getElementById("h2").style.display = "block";
   document.getElementById("image").style.display = "none";
+  display = "none";
 
   document.getElementById("reset").style.display = "block";
  
   document.getElementById("board").style.display = "grid";
   if (mode === "robot") {
     document.querySelector("h1").innerHTML = "當前模式: 單人模式";
+    resetBoard()
   }
 
   if (mode === "robotvsrobot") {
     document.querySelector("h1").innerHTML = "當前模式: 人工智慧間的對決";
     makeMove(document.getElementById("A"+String(parseInt(Math.random()*3 + 1))+String(parseInt(Math.random()*3 + 1))))
+    resetBoard()
   } 
   if (mode === "human") {
     document.querySelector("h1").innerHTML = "當前模式: 雙人模式";
+    resetBoard()
   }
 
   if (mode === "idiot") {
     document.querySelector("h1").innerHTML = "當前模式: 成就感模式";
+    resetBoard()
   }
 
   if (mode === "idiotvsidiot") {
     document.querySelector("h1").innerHTML = "當前模式: 人工智障間的對決";
     makeMove(document.getElementById("A"+String(parseInt(Math.random()*3 + 1))+String(parseInt(Math.random()*3 + 1))))
+    resetBoard()
   }
 }
 
@@ -76,14 +85,20 @@ function makeMove(cell) {
   OText = document.getElementById("inputer1").value
   XText = document.getElementById("inputer2").value
 
+  
+
   if (boardactive) {
     const index = Array.from(cell.parentNode.children).indexOf(cell);
 
   if (board[index] === "") {
     board[index] = currentPlayer;
     cell.textContent = currentPlayer;
-
-    if (checkWin(currentPlayer)) {
+    if (currentPlayer === OText) {
+      cell.style.color = "rgb(235,182,61)"
+    } else {
+      cell.style.color = "rgb(49,196,190)"
+    }
+    if (checkrealWin(currentPlayer)) {
       
         document.querySelector("h1").innerHTML = currentPlayer + " 勝利！";
         boardactive = false
@@ -139,9 +154,11 @@ function resetBoard() {
     currentPlayer = OText;
     board = ["", "", "", "", "", "", "", "", ""];
     const cells = document.querySelectorAll(".cell");
+
+    cells.forEach(cell => cell.style.color = "rgb(30,54,64)")
     cells.forEach(cell => cell.textContent = "");
 
-
+    
 
     if (modesave === "robot") {
         document.querySelector("h1").innerHTML = "當前模式: 單人模式";
@@ -167,7 +184,72 @@ function resetBoard() {
   }
 
 
+function colorcell(getcolorcell) {
+  console.log(getcolorcell);
+  getcolorcell.style.color = "rgb(212,30,30)";
+}
 
+async function checkcolorcell(whichtocolor,when) {
+  console.log(whichtocolor);
+  let getcell = parseInt(whichtocolor);
+
+  if (when == 1) {
+    await delay();
+  }
+
+  if (when == 2) {
+    await delay(0.35);
+  }
+
+  if (when == 3) {
+    await delay(0.7);
+  }
+
+  
+  let get = false;
+
+  if (getcell == 1) {
+    get = true;
+    colorcell(document.getElementById("A21"));
+}
+if (getcell == 2) {
+    get = true;
+    colorcell(document.getElementById("A31"));
+}
+if (getcell == 3) {
+    get = true;
+    colorcell(document.getElementById("A12"));
+}
+if (getcell == 4) {
+    get = true;
+    colorcell(document.getElementById("A22"));
+}
+if (getcell == 5) {
+    get = true;
+    colorcell(document.getElementById("A32"));
+}
+if (getcell == 6) {
+    get = true;
+    colorcell(document.getElementById("A13"));
+}
+if (getcell == 7) {
+    get = true;
+    colorcell(document.getElementById("A23"));
+}
+if (getcell == 8) {
+    get = true;
+    colorcell(document.getElementById("A33"));
+}
+
+if (get) {
+    
+} else {
+  colorcell(document.getElementById("A11"));
+}
+
+}
+
+  
 
 function minimax(newBoard, player) {
     const availableMoves = emptyCells(newBoard);
@@ -222,17 +304,56 @@ function minimax(newBoard, player) {
     return moves[bestMove];
   }
 
-function checkWin(player) {
+function checkWin(player,mode) {
     const winningCombos = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8], // 水平
       [0, 3, 6], [1, 4, 7], [2, 5, 8], // 垂直
       [0, 4, 8], [2, 4, 6] // 對角線
     ];
-  
+
+    
     for (const combo of winningCombos) {
-      if (board[combo[0]] === player && board[combo[1]] === player && board[combo[2]] === player) {
+      const [a, b, c] = combo;
+      
+      if (board[a] === player && board[b] === player && board[c] === player) {
+
+     
+
         return true;
+        
       }
+
+      
+    }
+    return false;
+  }
+
+
+
+  function checkrealWin(player) {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // 水平
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // 垂直
+      [0, 4, 8], [2, 4, 6] // 對角線
+    ];
+
+    
+    for (const combo of winningCombos) {
+      const [a, b, c] = combo;
+      
+      if (board[a] === player && board[b] === player && board[c] === player) {
+
+        checkcolorcell(a,1);
+        
+          checkcolorcell(b,2);
+        
+         checkcolorcell(c,3);
+
+        return true;
+        
+      }
+
+      
     }
     return false;
   }
@@ -243,14 +364,14 @@ function checkWin(player) {
   
  
   async function makeRobotMove() {
-    await delay(0.3);
+    await delay(0.5);
     const bestMove = minimax(board, XText).index;
     const cell = document.getElementById("board").children[bestMove];
     makeMove(cell);
   }
 
   async function makeDumbMove() {
-    await delay(0.3);
+    await delay(0.5);
     
     let randmmove = emptyCells(board)[parseInt(Math.random()*emptyCells(board).length)]
     let get = false
@@ -298,7 +419,7 @@ function checkWin(player) {
   }
 
   async function makeDumbMove2() {
-    await delay(0.3);
+    await delay(0.5);
     
     let randmmove = emptyCells(board)[parseInt(Math.random()*emptyCells(board).length)]
     let get = false
@@ -346,7 +467,7 @@ function checkWin(player) {
   }
 
   async function makeRobotMove2() {
-    await delay(0.3);
+    await delay(0.5);
     const bestMove = minimax(board, OText).index;
     const cell = document.getElementById("board").children[bestMove];
     makeMove(cell);
